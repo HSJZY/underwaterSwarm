f48ea77010e910e3680bf02ac3e1b795a4da63db
Zayvin <hnldlyjzy@163.com>
2018-06-13 03:25:09 +0000

    add udp communication to project

---
+++ udp_client.cpp
@@ -0,0 +1,41 @@
+#include "udp_client.h"
+
+udp_client::udp_client(string address,int port)
+{
+    /* 填写sockaddr_in*/
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(address.c_str());
+}
+
+void udp_client::start_listen()
+{
+    int sockfd, len = 0;
+    int addr_len = sizeof(struct sockaddr_in);
+    char buffer[1024];
+
+    /* 建立socket，注意必须是SOCK_DGRAM */
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("socket");
+        return ;
+    }
+
+    while(1) {
+        bzero(buffer, sizeof(buffer));
+        printf("Please enter a string to send to server: \n");
+        len = read(STDIN_FILENO, buffer, sizeof(buffer));
+
+        /* 将字符串传送给server端*/
+        sendto(sockfd, buffer, len, 0, (struct sockaddr *)&addr, addr_len);
+
+        /* 接收server端返回的字符串*/
+        len = recvfrom(sockfd, buffer, sizeof(buffer), 0,(struct sockaddr *)&addr, (socklen_t *)&addr_len);
+//        printf("Receive from server: %s\n", buffer);
+        string markers_pos=buffer;
+        cout<<"buffer:"<<buffer;
+    }
+
+
+}
---
+++ udp_client.h
@@ -0,0 +1,28 @@
+#ifndef UDP_CLIENT_H
+#define UDP_CLIENT_H
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<vector>
+#include <unistd.h>
+
+using namespace std;
+
+class udp_client
+{
+public:
+    udp_client(string address,int port);
+    void start_listen();
+private:
+    struct sockaddr_in addr;
+
+
+};
+
+#endif // UDP_CLIENT_H
